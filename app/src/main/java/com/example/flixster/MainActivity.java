fad23fb398ab9713c9aa4e192664b91f264c21f2
Nina Tang <ntang@fb.com>
2020-06-24 13:10:59 -0400

    Created Movie object and added parsing of Movie JSON from API to Movie objects.

--- app/src/main/java/com/example/flixster/MainActivity.java
+++ app/src/main/java/com/example/flixster/MainActivity.java
@@ -1,14 +1,56 @@
 package com.example.flixster;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.example.flixster.models.Movie;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
+
+import okhttp3.Headers;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static final String NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=bb03f20811abb1a4f08ad35fdbacf552";
+    public static final String TAG = "MainActivity";
+
+    List<Movie> movies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(NOW_PLAYING_URL, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                Log.d(TAG, "onSuccess");
+                JSONObject jsonObject = json.jsonObject;
+                try {
+                    JSONArray results = jsonObject.getJSONArray("results");
+                    //Log.i(TAG, "Results: "+results.toString());
+                    movies = Movie.fromJsonArray(results);
+                } catch (JSONException e) {
+                    Log.e(TAG, "Hit json exception", e);
+                    e.printStackTrace();
+                }
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                Log.d(TAG, "onSuccess");
+
+            }
+        });
     }
 }
